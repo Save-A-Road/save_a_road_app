0384aa77007d33baaf2a6d782ce59dfbab8294cd
wkdthf21 <wkdthf21@naver.com>
2019-11-16 23:57:05 +0900

    init setting

---
+++ app/src/main/java/com/example/save_a_road_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.save_a_road_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
